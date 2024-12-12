76978c30a52097e62fa16586d1b9314a0f09d64d
Christian Cadena <christiancadena@MacBook-Pro-de-Christian.local>
2024-12-11 23:49:47 -0600

    fix: fix deploy

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,11 +2,11 @@ import { InternetIdentityButton } from "@bundly/ares-react";
 
 export default function Header() {
   return (
-    <header className="bg-white">
+    <header className="bg-[#1e293b]">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <strong>ARES CONNECT</strong>
-        </div>
+        <span className="text-4xl font-normal text-[#E3EFFD] ">
+          Triourism
+        </span>
         <div className="lg:flex lg:gap-x-12"></div>
         <div className="lg:flex lg:flex-1 lg:justify-end">
           <InternetIdentityButton />
